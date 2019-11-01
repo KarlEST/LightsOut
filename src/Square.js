1bdce63fc72bc25a3efe66d54e74df31ea5f60c2
Karl Lääts <karl.laats@transferwise.com>
2019-11-01 17:01:01 +0100

    Use yarn, bump versions and add prettier and eslint

--- src/Square.js
+++ src/Square.js
@@ -3,23 +3,23 @@ import React, { Component } from 'react';
 import './Square.css';
 
 export default class Square extends Component {
-	static propTypes = {
-		square: PropTypes.number.isRequired,
-		x: PropTypes.number.isRequired,
-		y: PropTypes.number.isRequired,
-		onClick: PropTypes.func.isRequired,
-	};
+  static propTypes = {
+    square: PropTypes.number.isRequired,
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
 
-	render() {
-		const name = this.props.square ? 'square lighted' : 'square';
-		return <div className={name} onClick={this.handleClick} />;
-	}
+  render() {
+    const name = this.props.square ? 'square lighted' : 'square';
+    return <div className={name} onClick={this.handleClick} />;
+  }
 
-	handleClick = () => {
-		this.props.onClick({
-			squareValue: this.props.square,
-			x: this.props.x,
-			y: this.props.y,
-		});
-	};
+  handleClick = () => {
+    this.props.onClick({
+      squareValue: this.props.square,
+      x: this.props.x,
+      y: this.props.y,
+    });
+  };
 }
